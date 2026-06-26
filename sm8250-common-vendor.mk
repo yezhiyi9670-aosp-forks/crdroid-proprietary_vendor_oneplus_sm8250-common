fe09a8b324a4455d5d714d7f1c8101fab9d1ac3f
yezhiyi9670 <yezhiyi0562@foxmail.com>
2026-06-26 18:21:46 +0800

    sm8250-common: De-blob oplus interfaces
    Change-Id: I86c24df31b72a9058ba3f2430f5c1bb469c4fd0b

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -2033,7 +2033,6 @@ PRODUCT_PACKAGES += \
     vendor.display.color@1.5 \
     vendor.display.postproc@1.0 \
     vendor.oplus.hardware.cammidasservice@1.0 \
-    vendor.oplus.hardware.radio-V2-ndk_platform \
     vendor.qti.data.factory@2.0 \
     vendor.qti.data.factory@2.1 \
     vendor.qti.data.factory@2.2 \
@@ -2190,7 +2189,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.imsrtpservice@3.1 \
     libfastov_dsp \
     vendor.oplus.hardware.orms@1.0 \
-    vendor.oplus.hardware.performance-V1-ndk_platform \
     android.hardware.secure_element@1.0-impl \
     com.qti.stats.pdlib_odm \
     libipebpsstriping_odm \
@@ -2244,13 +2242,9 @@ PRODUCT_PACKAGES += \
     vendor.dolby_v3_6.hardware.dms360@2.0-impl \
     vendor.dolby_v3_6.hardware.dms360@2.0 \
     vendor.oplus.hardware.appradio@1.0 \
-    vendor.oplus.hardware.cammidasservice-V1-ndk_platform \
-    vendor.oplus.hardware.commondcs-V1-ndk_platform \
     vendor.oplus.hardware.communicationcenter_compat@1.0 \
     vendor.oplus.hardware.ims@1.0 \
-    vendor.oplus.hardware.olc2-V1-ndk_platform \
     vendor.oplus.hardware.oplusSensor@1.0 \
-    vendor.oplus.hardware.osense.client-V1-ndk_platform \
     vendor.oplus.hardware.osense.client@1.0 \
     vendor.oplus.hardware.radio@1.0 \
     vendor.pixelworks.hardware.display@1.0 \
